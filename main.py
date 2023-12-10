f9f8a817dde6f50a249cf88aec601c9d6d800ca4
bthee <b.theeck08@gmail.com>
2023-12-10 16:46:25 +0100

    Added function to count individual letters from book string

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_char = get_num_char(text)
     print(f"{num_words} words in the document")
+    print(num_char)
 
 
 def get_book_text(book_path):
@@ -13,6 +15,16 @@ def get_book_text(book_path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+
+def get_num_char(text):
+    chars = {}
+    for char in text:
+        if char in chars:
+            chars[char.lower()] += 1
+        else:
+            chars[char.lower()] = 1
+    return chars
 
 
 if __name__ == "__main__":
