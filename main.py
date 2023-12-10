307d85dabb6a154e197eb10073cf74cef3ae7301
bthee <b.theeck08@gmail.com>
2023-12-10 22:05:50 +0100

    Added function that counts individual letters

--- main.py
+++ main.py
@@ -1,31 +1,47 @@
 # The main function serves as the entry point for the program.
-def main():
+def main()-> int:
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    num_char = get_num_char(text)
+    text_lower = text.lower()
+    num_words = get_num_words(text_lower)
+    num_char = get_num_char(text_lower)
+    sorted_char = sort_char(num_char)
     print(f"{num_words} words in the document")
-    print(num_char)
+    print()
+    for i in sorted_char:
+        print(f"The '{i[1]}' character was found {i[0]} times")
 
 # Function to read the text content of a book from a given file path.
-def get_book_text(book_path):
+def get_book_text(book_path: str)-> str:
     with open(book_path) as f:
         return f.read()
 
 # Function to count the number of words in a given text.
-def get_num_words(text):
+def get_num_words(text: str)-> int:
     words = text.split()
     return len(words)
 
 # Function to count the occurrences of each character in a given text.
-def get_num_char(text):
+def get_num_char(text: str)-> dict:
     chars = {}
     for char in text:
         if char in chars:
-            chars[char.lower()] += 1
+            chars[char] += 1
         else:
-            chars[char.lower()] = 1
+            chars[char] = 1
     return chars
+
+# 
+def sort_char(sorted: dict)-> list:
+    cleaned_list = []
+    key = list(sorted.keys())
+    value = list(sorted.values())
+    for key, value in sorted.items():
+        if key.isalpha():
+            cleaned_list.append((value, key))
+    cleaned_list.sort(reverse=True)
+    return cleaned_list
+
 
 # Check if the script is being run as the main program.
 if __name__ == "__main__":
