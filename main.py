d4e96d8e9563534664de5e2b529e0f5ecd2abfdf
bthee <b.theeck08@gmail.com>
2023-12-10 16:50:50 +0100

    Added comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# The main function serves as the entry point for the program.
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -6,17 +7,17 @@ def main():
     print(f"{num_words} words in the document")
     print(num_char)
 
-
+# Function to read the text content of a book from a given file path.
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
 
-
+# Function to count the number of words in a given text.
 def get_num_words(text):
     words = text.split()
     return len(words)
 
-
+# Function to count the occurrences of each character in a given text.
 def get_num_char(text):
     chars = {}
     for char in text:
@@ -26,6 +27,6 @@ def get_num_char(text):
             chars[char.lower()] = 1
     return chars
 
-
+# Check if the script is being run as the main program.
 if __name__ == "__main__":
     main()
